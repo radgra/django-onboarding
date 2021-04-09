a6ec0bda56bd580fd8e969452981f4f22ab2fea2
Radoslaw Grabos <radgra2@gmail.com>
2021-04-09 14:47:51 +0200

    delete unused code

--- core/admin.py
+++ core/admin.py
@@ -10,26 +10,6 @@ class TemplateTasksInline(admin.TabularInline):
     extra = 0
     ordering = ('position',)
 
-    
-# Register your models here.
-
-
-# @admin.register(models.Contract)
-# class ContractAdmin(admin.ModelAdmin):
-#     pass
-
-
-# @admin.register(models.NewEmployee)
-# class NewEmployeeAdmin(admin.ModelAdmin):
-#     pass
-
-
-
-# Feature will be added later
-# @admin.register(models.OnboardingTemplate)
-# class OnboardingTemplateAdmin(admin.ModelAdmin):
-#     pass
-
 
 
 @admin.register(models.OnboardingTemplate)
